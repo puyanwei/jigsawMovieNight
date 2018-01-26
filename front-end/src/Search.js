6912c61b00989d2f8040ea5a4eb04c5d0a666fa7
Puyan Wei <puyanwei@gmail.com>
2018-01-26 15:08:53 +0000

    created Search class, and rendered to page

---
+++ front-end/src/Search.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Search extends Component {
+  render() {
+    return(
+
+       <div className="Search">
+      <h2>Search for a Movie</h2>
+       </div>
+
+
+    )
+  }
+}
+
+export default Search
