7a1bdbc322cae18cc9fb3fa40eac825c0e3d6dd1
Puyan Wei <puyanwei@gmail.com>
2018-01-26 15:22:02 +0000

    AD add text box and button for search

--- front-end/src/Search.js
+++ front-end/src/Search.js
@@ -6,6 +6,12 @@ class Search extends Component {
 
        <div className="Search">
       <h2>Search for a Movie</h2>
+       <div>
+       <form className="searchBox" onSubmit={this.onFormSubmit}>
+       <input type="text" name="userInput" value="text" />
+       <button type="submit">Search</button>
+       </form>
+       </div>
        </div>
 
 
